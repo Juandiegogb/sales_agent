659c2f408193c561da28ab2d5b0470ba127b4225
Juan Diego <juandiegar@gmail.com>
2025-07-21 09:22:57 -0500

    Start project

---
+++ apps/products/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Brand,Group,Product,Rag
+
+
+admin.site.register([Brand,Group,Product,Rag])
---
+++ main.py
@@ -0,0 +1,75 @@
+from langchain_community.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain_postgres import PGVector
+from dotenv import load_dotenv
+from sqlalchemy import true
+from pprint import pprint
+
+from langchain_core.documents import Document
+
+docs = [
+    Document(
+        page_content="there are cats in the pond",
+        metadata={"id": 1, "location": "pond", "topic": "animals"},
+    ),
+    Document(
+        page_content="ducks are also found in the pond",
+        metadata={"id": 2, "location": "pond", "topic": "animals"},
+    ),
+    Document(
+        page_content="fresh apples are available at the market",
+        metadata={"id": 3, "location": "market", "topic": "food"},
+    ),
+    Document(
+        page_content="the market also sells fresh oranges",
+        metadata={"id": 4, "location": "market", "topic": "food"},
+    ),
+    Document(
+        page_content="the new art exhibit is fascinating",
+        metadata={"id": 5, "location": "museum", "topic": "art"},
+    ),
+    Document(
+        page_content="a sculpture exhibit is also at the museum",
+        metadata={"id": 6, "location": "museum", "topic": "art"},
+    ),
+    Document(
+        page_content="a new coffee shop opened on Main Street",
+        metadata={"id": 7, "location": "Main Street", "topic": "food"},
+    ),
+    Document(
+        page_content="the book club meets at the library",
+        metadata={"id": 8, "location": "library", "topic": "reading"},
+    ),
+    Document(
+        page_content="the library hosts a weekly story time for kids",
+        metadata={"id": 9, "location": "library", "topic": "reading"},
+    ),
+    Document(
+        page_content="a cooking class for beginners is offered at the community center",
+        metadata={"id": 10, "location": "community center", "topic": "classes"},
+    ),
+]
+
+
+load_dotenv()
+
+
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+
+
+connection = "postgresql+psycopg://postgres:Admin@localhost:5500/vector_learning"
+
+
+vector_store = PGVector(
+    embeddings=embeddings,
+    collection_name="my_docs",
+    connection=connection,
+    use_jsonb=True,
+)
+
+# vector_store.add_documents(docs, ids=[doc.metadata["id"] for doc in docs])
+# vector_store.add_documents(texts)
+
+
+pprint(vector_store.similarity_search_with_score("in the pond", k=10))
